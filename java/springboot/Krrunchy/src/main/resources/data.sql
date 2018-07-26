28c0a3900117fc62f5736374d89aa1e099f03b10
Zoher <zoher.surka@gmail.com>
2018-07-27 00:04:43 +0530

    Added data.sql file for the h2 database

---
+++ java/springboot/Krrunchy/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `menu_schedule` (`menu_id`, `delivery_date`) VALUES (1, '2018-7-21');
+INSERT INTO `menu_schedule` (`menu_id`, `delivery_date`) VALUES (2, '2018-7-22');
+
+INSERT INTO `menu_details` (`menu_id`, `items`) VALUES (1, 'Salad, curry, roti');
+INSERT INTO `menu_details` (`menu_id`, `items`) VALUES (2, 'dal, jeera rice');
+INSERT INTO `menu_details` (`menu_id`, `items`) VALUES (3, 'mix veg curry, roti, plain rice');
+INSERT INTO `menu_details` (`menu_id`, `items`) VALUES (4, 'Khaboos, paya ,khichadi');
